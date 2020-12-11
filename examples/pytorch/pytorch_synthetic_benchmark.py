e477b98cae99976a512195b7af4f8465c97d41dd
Guoshuai Zhao <guzhao@microsoft.com>
2020-12-11 19:45:46 +0900

    add half precision

--- examples/pytorch/pytorch_synthetic_benchmark.py
+++ examples/pytorch/pytorch_synthetic_benchmark.py
@@ -53,78 +53,82 @@ MODEL_LIST = {
     models.shufflenetv2:models.shufflenetv2.__all__[1:]
 }
 
-for model_type in MODEL_LIST.keys():
-    for model_name in MODEL_LIST[model_type]:
-        # Set up standard model.
-        model = getattr(models, model_name)()
-        
-        # By default, Adasum doesn't need scaling up learning rate.
-        lr_scaler = hvd.size() if not args.use_adasum else 1
-        
-        if args.cuda:
-            # Move model to GPU.
-            model.cuda()
-            # If using GPU Adasum allreduce, scale learning rate by local_size.
-            if args.use_adasum and hvd.nccl_built():
-                lr_scaler = hvd.local_size()
-        
-        optimizer = optim.SGD(model.parameters(), lr=0.01 * lr_scaler)
-        
-        # Horovod: (optional) compression algorithm.
-        compression = hvd.Compression.fp16 if args.fp16_allreduce else hvd.Compression.none
-        
-        # Horovod: wrap optimizer with DistributedOptimizer.
-        optimizer = hvd.DistributedOptimizer(optimizer,
-                                             named_parameters=model.named_parameters(),
-                                             compression=compression,
-                                             op=hvd.Adasum if args.use_adasum else hvd.Average)
-        
-        # Horovod: broadcast parameters & optimizer state.
-        hvd.broadcast_parameters(model.state_dict(), root_rank=0)
-        hvd.broadcast_optimizer_state(optimizer, root_rank=0)
-        
-        # Set up fixed fake data
-        data = torch.randn(args.batch_size, 3, 224, 224)
-        target = torch.LongTensor(args.batch_size).random_() % 1000
-        if args.cuda:
-            data, target = data.cuda(), target.cuda()
-        
-        
-        def benchmark_step():
-            optimizer.zero_grad()
-            output = model(data)
-            loss = F.cross_entropy(output, target)
-            loss.backward()
-            optimizer.step()
-        
-        
-        def log(s, nl=True):
-            if hvd.rank() != 0:
-                return
-            print(s, end='\n' if nl else '')
-        
-        
-        log('Model: %s' % model_name)
-        log('Batch size: %d' % args.batch_size)
-        device = 'GPU' if args.cuda else 'CPU'
-        log('Number of %ss: %d' % (device, hvd.size()))
-        
-        # Warm-up
-        log('Running warmup...')
-        timeit.timeit(benchmark_step, number=args.num_warmup_batches)
-        
-        # Benchmark
-        log('Running benchmark...')
-        img_secs = []
-        for x in range(args.num_iters):
-            time = timeit.timeit(benchmark_step, number=args.num_batches_per_iter)
-            img_sec = args.batch_size * args.num_batches_per_iter / time
-            log('Iter #%d: %.1f img/sec per %s' % (x, img_sec, device))
-            img_secs.append(img_sec)
-        
-        # Results
-        img_sec_mean = np.mean(img_secs)
-        img_sec_conf = 1.96 * np.std(img_secs)
-        log('Img/sec per %s: %.1f +-%.1f' % (device, img_sec_mean, img_sec_conf))
-        log('Total img/sec on %d %s(s): %.1f +-%.1f' %
-            (hvd.size(), device, hvd.size() * img_sec_mean, hvd.size() * img_sec_conf))
+precisions = ["float", "half"]
+for precision in precisions:
+    for model_type in MODEL_LIST.keys():
+        for model_name in MODEL_LIST[model_type]:
+            # Set up standard model.
+            model = getattr(model_type, model_name)()
+            model = getattr(model, precision)()
+            
+            # By default, Adasum doesn't need scaling up learning rate.
+            lr_scaler = hvd.size() if not args.use_adasum else 1
+            
+            if args.cuda:
+                # Move model to GPU.
+                model.cuda()
+                # If using GPU Adasum allreduce, scale learning rate by local_size.
+                if args.use_adasum and hvd.nccl_built():
+                    lr_scaler = hvd.local_size()
+            
+            optimizer = optim.SGD(model.parameters(), lr=0.01 * lr_scaler)
+            
+            # Horovod: (optional) compression algorithm.
+            compression = hvd.Compression.fp16 if args.fp16_allreduce else hvd.Compression.none
+            
+            # Horovod: wrap optimizer with DistributedOptimizer.
+            optimizer = hvd.DistributedOptimizer(optimizer,
+                                                 named_parameters=model.named_parameters(),
+                                                 compression=compression,
+                                                 op=hvd.Adasum if args.use_adasum else hvd.Average)
+            
+            # Horovod: broadcast parameters & optimizer state.
+            hvd.broadcast_parameters(model.state_dict(), root_rank=0)
+            hvd.broadcast_optimizer_state(optimizer, root_rank=0)
+            
+            # Set up fixed fake data
+            data = torch.randn(args.batch_size, 3, 224, 224)
+            target = torch.LongTensor(args.batch_size).random_() % 1000
+            if args.cuda:
+                data, target = data.cuda(), target.cuda()
+            
+            
+            def benchmark_step():
+                optimizer.zero_grad()
+                output = model(data)
+                loss = F.cross_entropy(output, target)
+                loss.backward()
+                optimizer.step()
+            
+            
+            def log(s, nl=True):
+                if hvd.rank() != 0:
+                    return
+                print(s, end='\n' if nl else '')
+            
+            
+            log('Model: %s' % model_name)
+            log('Precision: %s' % precision)
+            log('Batch size: %d' % args.batch_size)
+            device = 'GPU' if args.cuda else 'CPU'
+            log('Number of %ss: %d' % (device, hvd.size()))
+            
+            # Warm-up
+            log('Running warmup...')
+            timeit.timeit(benchmark_step, number=args.num_warmup_batches)
+            
+            # Benchmark
+            log('Running benchmark...')
+            img_secs = []
+            for x in range(args.num_iters):
+                time = timeit.timeit(benchmark_step, number=args.num_batches_per_iter)
+                img_sec = args.batch_size * args.num_batches_per_iter / time
+                log('Iter #%d: %.1f img/sec per %s' % (x, img_sec, device))
+                img_secs.append(img_sec)
+            
+            # Results
+            img_sec_mean = np.mean(img_secs)
+            img_sec_conf = 1.96 * np.std(img_secs)
+            log('Img/sec per %s: %.1f +-%.1f' % (device, img_sec_mean, img_sec_conf))
+            log('Total img/sec on %d %s(s): %.1f +-%.1f' %
+                (hvd.size(), device, hvd.size() * img_sec_mean, hvd.size() * img_sec_conf))
